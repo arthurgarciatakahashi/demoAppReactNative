f0737cffb8b264e943bfa22bba8e848d72cf99e9
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2024-03-07 19:14:45 -0300

    create home

---
+++ my-project/src/screens/Home/index.js
@@ -0,0 +1,102 @@
+import { View, StyleSheet, Text, Image, TouchableOpacity, FlatList, ScrollView } from "react-native";
+import PrimeVideoLogo from "../../assets/prime_video.png";
+import AmazonLogo from "../../assets/amazon_logo.png";
+import MovieTheWhell from "../../assets/movies/the_wheel_of_time.png"
+import { MOVIESWATCHING } from "../../utils/moviesWatching";
+import { MoviesCard } from "../../components/MoviesCard";
+
+export const Home = () => {
+    return (
+
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image style={styles.primeLogoImg} source={PrimeVideoLogo} />
+                <Image style={styles.amazonLogoImg} source={AmazonLogo} />
+            </View>
+
+            <View style={styles.category}>
+                <TouchableOpacity>
+                    <Text style={styles.categoryText}>Home</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.categoryText}>TV Shows</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.categoryText}>Movies</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.categoryText}>Kids</Text>
+                </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity style={styles.movieImageThumbnail}>
+                <Image style={styles.movieImage} source={MovieTheWhell} />
+            </TouchableOpacity>
+            <ScrollView>
+                <Text style={styles.moviesText}>Continue Watching</Text>
+                <FlatList
+                    data={MOVIESWATCHING}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => <MoviesCard movieURL={item.moviesURL} />}
+                    horizontal
+                    contentContainerStyle={styles.contentList}
+                    showsHorizontalScrollIndicator={false}
+                />
+            </ScrollView>
+
+
+
+        </View>
+    );
+
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#232F3E",
+        alignItems: "flex-start",
+    },
+
+    header: {
+        width: "100%",
+        paddingTop: 80,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    amazonLogoImg: {
+        marginTop: -32,
+        marginLeft: 30,
+    },
+    primeLogoImg: {
+
+    },
+    category: {
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        marginTop: 30,
+        marginBottom: 15,
+    },
+    categoryText: {
+        fontSize: 14,
+        fontWeight: "700",
+        color: "#FFf"
+    },
+    moviesText: {
+        color: "#ffff",
+        fontSize: 18,
+        fontWeight: "700",
+        padding: 15,
+
+    },
+    movieImageThumbnail: {
+        width: "100%",
+        alignItems: "center",
+    },
+    contentList: {
+        paddingLeft: 18,
+        paddingRight: 30,
+    },
+
+});
